72be71aa2fe1bcefe8745b82a076a4800956ceb8
seewoelfin <45685131+seewoelfin@users.noreply.github.com>
2023-06-09 15:56:03 +0200

    nu aber auch mit schicker variablenbenennung

--- src/main.py
+++ src/main.py
@@ -1,27 +1,37 @@
 import gym
 from gym.envs.registration import register
-from stable_baselines3 import PPO
+from stable_baselines3 import DQN
 from stable_baselines3.ppo.policies import MlpPolicy
 
 # Funktion zum Trainieren der KI
 def train_agent(env, num_steps):
-    model = PPO(MlpPolicy, env, verbose=1)
+    model = DQN(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        tensorboard_log="./DQN_tensorboard/",
+        policy_kwargs=dict(net_arch=[256, 256]),
+        learning_starts=10,
+        batch_size=512,
+        learning_rate=4e-3,
+    )
     model.learn(total_timesteps=num_steps)
+    #model = A2C('MlpPolicy', env, verbose=1)
+    #model.learn(total_timesteps=num_steps)
     return model
 
 # Funktion zum Testen der KI
-def test_agent(model, env, num_episodes):
-    for _ in range(num_episodes):
+def test_agent(model, env, num__test_episodes):
+    for ep in range(num_test_episodes):
         obs = env.reset()
         done = False
-        total_reward = 0
-
-    while not done:
-        action, _ = model.predict(obs, deterministic=True)
-        obs, reward, done, _ = env.step(action)
-        total_reward += reward
-
-    print(f"Total reward: {total_reward}")
+        while not done:
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = env.step(action)
+            if (rewards == 1000)\
+                    :
+                print(f"Spiel in Episode {ep} gewonnen.")
+                env.render()
 
 # Hauptprogramm
 if __name__ == "__main__":
@@ -30,11 +40,13 @@ if __name__ == "__main__":
         entry_point='minesweeper.minesweeper:MinesweeperDiscreetEnv',
     )
     env = gym.make("MinesweeperDiscreet-v0")
-    num_steps = 1e9
-    num_episodes = 50
+    # das was er trainiert
+    num_steps = 100000
+    # das was er am Ende spielt
+    num_test_episodes = 10000
 
     print("Training agent...")
     trained_model = train_agent(env, num_steps)
 
     print("Testing agent...")
-    test_agent(trained_model, env, num_episodes)
+    test_agent(trained_model, env, num_test_episodes)
