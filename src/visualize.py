9a898cb9cd4cc4d4bd6d24fd76cd32c8eeaab6f2
seewoelfin <45685131+seewoelfin@users.noreply.github.com>
2023-07-05 16:25:17 +0200

    n basic visualisierungsgedöns, diesmal cooler und bunt

--- src/visualize.py
+++ src/visualize.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 firstData = pd.read_csv("./csv/run-PPO_71-tag-rollout_ep_rew_mean.csv")
 secondData = pd.read_csv("./csv/run-PPO_72-tag-rollout_ep_rew_mean.csv")
 
 # Ausreißer nach Interquartilsabstandsmethode entfernen
-firstQ1 = firstData.quantile(q=.25)
-firstQ3 = firstData.quantile(q=.75)
+firstQ1 = np.percentile(firstData['Value'], 25)
+firstQ3 = np.percentile(firstData['Value'], 75)
+firstIQR = stats.iqr(firstData['Value'])
 
+firstData_clean = firstData[~((firstData['Value'] < firstQ1 - 1.5 * firstIQR) | (firstData['Value'] > firstQ3 + 1.5 * firstIQR))]
+
+secondQ1 = np.percentile(secondData['Value'], 25)
+secondQ3 = np.percentile(secondData['Value'], 75)
+secondIQR = stats.iqr(secondData['Value'])
+
+secondData_clean = secondData[~((secondData['Value'] < secondQ1 - 1.5 * secondIQR) | (secondData['Value'] > secondQ3 + 1.5 * secondIQR))]
 
 # Boxplot
 plt.subplot(1, 2, 1)
@@ -24,30 +33,33 @@ data = [firstData['Value'], secondData['Value']]
 plt.boxplot(data)
 plt.xticks([1, 2], ['Hoch zu niedrig', 'Niedrig zu hoch'])
 plt.title("Dynamische Lernrate")
-
 plt.show()
 
 # Scatter
-x_scatter1 = np.array(firstData['Step'])
-y_scatter1 = np.array(firstData['Value'])
+x_scatter1 = np.array(firstData_clean['Step'])
+y_scatter1 = np.array(firstData_clean['Value'])
 
-x_scatter2 = np.array(secondData['Step'])
-y_scatter2 = np.array(secondData['Value'])
+x_scatter2 = np.array(secondData_clean['Step'])
+y_scatter2 = np.array(secondData_clean['Value'])
 
 plt.figure()
-plt.scatter(x_scatter1, y_scatter1, label='Hoch zu niedrig')
-plt.scatter(x_scatter2, y_scatter2, label='Niedrig zu hoch')
-
+plt.scatter(x_scatter1, y_scatter1, label='Hoch zu niedrig', color='orange')
+plt.scatter(x_scatter2, y_scatter2, label='Niedrig zu hoch', color='blueviolet')
+plt.xlabel('Episoden Reward')
+plt.ylabel('Anzahl in Tsd.')
+plt.title('Dynamische Lernrate')
 plt.legend()
 plt.show()
 
 # Histogram
-histo1 = np.array(firstData['Value'])
-histo2 = np.array(secondData['Value'])
+histo1 = np.array(firstData_clean['Value'])
+histo2 = np.array(secondData_clean['Value'])
 
 plt.figure()  # Neue Figure erstellen
-plt.hist(histo1, label='Hoch zu niedrig')
-plt.hist(histo2, label='Niedrig zu hoch')
-
+plt.xlabel('Episoden Reward')
+plt.ylabel('Anzahl in Tsd.')
+plt.title('Dynamische Lernrate')
+plt.hist(histo1, label='Hoch zu niedrig', color='orange')
+plt.hist(histo2, label='Niedrig zu hoch', color='blueviolet')
 plt.legend()
 plt.show()
